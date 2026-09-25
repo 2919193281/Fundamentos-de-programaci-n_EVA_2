274bf7331d0e7e1c6dec2ec104b6f80e321be81d
Jesús Emanuel <L26550308@chihuahua2.TECNM.MX>
2026-09-24 19:04:24 -0500

    Add files via upload

---
+++ EVA2_7_ELSE_IF2/src/eve2_7_else_if2/EVE2_7_ELSE_IF2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eve2_7_else_if2;
+
+/**
+ *
+ * @author bisonte
+ */
+public class EVE2_7_ELSE_IF2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
